cc2aae58b94268c77a453e8b8927853b7b5e2a43
Peter Knight <cathedrow@icloud.com>
2020-11-20 15:43:21 +0000

    Add GPS ephemeris fetch

---
+++ gps_ephemeris.py
@@ -0,0 +1,29 @@
+import datetime
+import requests
+import time
+import georinex as gr
+
+def ephemeris_filename(ephemeris_date):
+    # Return daily filename
+    return ephemeris_date.strftime("brdc%j0.%yn.Z")
+
+def get_ephemeris(ephemeris_date):
+    fn = epehmeris_filename('brdc%j0.%yn.Z')
+    try:
+        print("Looking for file {}".format(fn))
+        open(fn, 'rb').close()
+    except FileNotFoundError:
+        # Attempt download from NASA if not locally available
+        # TODO: Add authentication required for this to work.
+        raise NotImplementedError
+        url = (
+            "https://cddis.nasa.gov/archive/gnss/data/daily/" +
+            ephemeris_date.strftime("%Y/%j/%yn/brdc%j0.%yn.Z")
+        )
+        response = requests.get(url)
+        if response.status_code >= 300:
+            response.raise_for_status()
+        with open(fn, 'wb') as fh:
+            fh.write(response.content)
+        time.sleep(1)  # Simple rate limiting
+    return gr.load(fn)
