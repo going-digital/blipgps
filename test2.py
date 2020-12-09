492a1375073f500e5f42eeaf942d8d04c7ea6073
Peter Knight <peter.knight@cefastechnology.co.uk>
2020-12-09 15:54:38 +0000

    Implement autodownload of sample file
    Also add titles to charts, and widen doppler.

--- test2.py
+++ test2.py
@@ -7,19 +7,40 @@ import pyfftw
 np.fft = pyfftw.interfaces.numpy_fft
 pyfftw.interfaces.cache.enable()
 from gps_chip import ca_table
-#%%
+import requests
+import os
+import gzip
+import shutil
+# %%
+# Download test file if necessary.
+# This file is 1.5GB long when decompressed.
 fn = "2013_04_04_GNSS_SIGNAL_at_CTTC_SPAIN.dat"
-fc = 1575.42e6
-fs = 4e6
-chip_samples = int(fs//1000)
-offset = 1000000
+if not os.path.exists(fn):
+    print("Downloading GPS sample file. This will take some time...")
+    url = "https://sourceforge.net/projects/gnss-sdr/files/data/2013_04_04_GNSS_SIGNAL_at_CTTC_SPAIN.tar.gz"
+    r = requests.get(url, stream=True)
+    total_size = int(r.headers.get('content-length', 0))
+    with gzip.open(r.raw, 'rb') as f_in:
+        with open(fn, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    print("Download complete")
+# %%
+# Set up inputs to algorithm
+fc = 1575.42e6  # Assumed centre frequency of receiver
+fs = 4e6 # Sample rate of receiver
+chip_samples = int(fs//1000) # Samples per chip (a GPS chip is exactly 1ms long)
+offset = 0
 num_chips = 2
 count = num_chips * chip_samples
 data = np.fromfile(fn, dtype=np.int16, count=int(offset+count)*2)
+# Quantise to 2 bits I and 2 bits Q per sample
 data = np.clip(np.floor_divide(data, 150), -2, 1) + 0.5
+# Convert to complex numpy array
 data = np.reshape(data, (data.shape[0]//2, 2))
 data = data[offset:, 0] + 1j * data[offset:, 1]
 #%%
+# Precalculate reference tables
+# These are reference samples of all known GPS chips.
 ca_chip_table = ca_table(int(fs))
 chip_window = min(10, num_chips//2)
 fft_ca_chips = {
@@ -32,13 +53,16 @@ fft_ca_chips = {
     )) for (i, ca) in ca_chip_table.items()
 }
 #%%
-# doppler in range -10000 - 10000 Hz
+# GPS should see doppler of +-5kHz, but there will be significant carrier
+# error due to the local receiver. 1ppm = 1.5kHz at these frequencies.
+# doppler search windows is +-15kHz
+#
 # FFT frequency bins are -fs/2 to +fs/2
 # FFT length is chip_samples*num_chips
 # So for fs=4MHz, bins are fs/(chip_samples*num_chips) apart
 
 from mpl_toolkits.mplot3d import Axes3D
-doppler_offset = 15000
+doppler_offset = 30000
 xx = np.arange(chip_samples)/chip_samples
 yy = np.arange(-doppler_offset, doppler_offset+1, 250)
 
@@ -65,8 +89,15 @@ for prn in tqdm(fft_ca_chips.keys()):
         # Plot matches over doppler and phase
         fig, axs = plt.subplots(1,2,figsize=(10,5))
         axs[0].plot(xx, results_phase)
+        axs[0].set_title("PRN {} phase {} us".format(
+            prn,
+            1000*xx[np.argmax(results_phase)]
+        ))
         axs[1].plot(yy, results_doppler)
-        plt.title("PRN {}".format(prn))
+        axs[1].set_title("PRN {} offset {} Hz".format(
+            prn,
+            yy[np.argmax(results_doppler)]
+        ))
         plt.show()
 
 # %%
